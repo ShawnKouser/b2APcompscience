3c848f64d25736b26b4dc1b8d25efe09a3755ad1
ShawnKouser <email.johnhouser@gmail.com>
2017-09-22 17:05:06 -0600

    just a simple change. 2nd attempt

--- GITtest/src/GitText.java
+++ GITtest/src/GitText.java
@@ -3,7 +3,8 @@ public class GitText {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("A git test");
+		System.out.println("a Line 2");
 	}
 
 }
